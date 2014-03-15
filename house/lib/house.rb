dfe43b1d7986263849f124b69fd1e1d3346e9c16
Luke Morris <luke@imfuna.com>
2014-03-15 00:20:04 -0400

    Improve readability, implement default dependencies.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,32 +1,30 @@
 class Bard
   attr_reader :phrases
-  def initialize(memory)
+  def initialize(memory=Memory.new)
     @phrases = memory.recall
   end
 
   def to_s
-    phrases.length.times.map{|number| verse(number+1)}.join
-  end
-
-  def verse(number)
-    'This is ' + phrases.last(number).join(' ') + ".\n\n"
+    (1..phrases.size).map do |number| 
+      'This is ' + phrases.last(number).join(' ') + ".\n\n"
+    end.join
   end
 end
 
 class Memory
   attr_reader :actors, :actions
   def initialize(actors=ACTORS, actions=ACTIONS)
-    @actors, @actions = pre_initialize(actors, actions)
+    @actors, @actions = determine_order(actors, actions)
   end
 
-  def pre_initialize(actors, actions)
+  def determine_order(actors, actions)
     [actors, actions]
   end
 
   def recall
-    actors.zip(actions).map { |actor, action|
+    actors.zip(actions).map do |actor, action|
       'the ' + actor + ' that ' + action
-    } << 'the house that Jack built'
+    end << 'the house that Jack built'
   end
 
   ACTORS = ['horse and the hound and the horn','farmer sowing his corn','rooster that crowed in the morn','priest all shaven and shorn','man all tattered and torn','maiden all forlorn','cow with the crumpled horn','dog','cat','rat','malt']
@@ -34,7 +32,7 @@ class Memory
 end
 
 class ImpairedMemory < Memory
-  def pre_initialize(actors, actions)
+  def determine_order(actors, actions)
     [actors.shuffle, actions.shuffle]
   end
 end
